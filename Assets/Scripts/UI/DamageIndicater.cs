8a865be1827ce17b2dcbc5b2e0302f7d57581554
KimSungHo-web <epdlam20@gmail.com>
2024-10-28 12:38:57 +0900

    (docs)DamageIndicater.cs

--- Assets/Scripts/UI/DamageIndicater.cs
+++ Assets/Scripts/UI/DamageIndicater.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class DamageIndicater : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Image image;
+    public float flatSpeed;
+    
+
+    private Coroutine coroutine;
     void Start()
     {
-        
+        CharacterManager.Instance.Player.condition.onTakeDamage += Flash;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Flash() 
     {
-        
+        if (coroutine != null) 
+        {
+            StopCoroutine(coroutine);
+        }
+
+        image.enabled = true;
+        image.color= new Color(1f, 100f / 255, 100f / 255f);
+        coroutine = StartCoroutine(FadeAway());
+    }
+
+    private IEnumerator FadeAway() 
+    {
+        float starAlpha = 0.3f;
+        float a=starAlpha;
+
+        while (a > 0) 
+        {
+            a-=(starAlpha/flatSpeed)*Time.deltaTime;
+            image.color = new Color(1f,100f/255,100f/255f,a);
+                yield return null;
+        }
+
+        image.enabled = false;
     }
 }
